5c7a5425036a761fc05e14dcc8319a13d701a75e
Inge Chiles <inge.chiles@gmail.com>
2024-05-04 15:33:44 -0500

    Initial Commit

---
+++ DogBook/DogBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBookApp.swift
+//  DogBook
+//
+//  Created by Ingrid K Chiles on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DogBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
